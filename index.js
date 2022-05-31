9ef134d77741c94be02f9a14e19abb6aaf4265cd
LAPTOP-HTDN1JE0\Oriiromero <oriana.nahir.r@gmai.com>
2022-05-31 10:33:35 +0200

    [css] burguer toggle [js] burguer toggle animation

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ const navSlide = function()
                      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
                  }
             });
+
+            //Burguer animation
+            burguer.classList.toggle("toggle");
+
         });
- 
 }
 
 navSlide();
