168196ef9455e753a326a4cf4eac938542c199f1
LAPTOP-HTDN1JE0\Oriiromero <oriana.nahir.r@gmai.com>
2022-06-08 20:36:57 +0200

    [html] add skills section [css] adding styles [js] adding animation

--- index.js
+++ index.js
@@ -67,3 +67,35 @@ rightArrow.addEventListener("click", function(){
   setIndex();
   indicatorParents.children[sectionIndex].classList.add("selected");
 });
+
+
+//SKILSS BAR ANIMATION//
+const skillsSection = document.getElementById("skills-section");
+const progressBars = document.querySelectorAll(".progress-bar");
+
+
+function showProgress(){
+  progressBars.forEach(progressBar => {
+    const value = progressBar.dataset.progress;
+    progressBar.style.opacity = 1;
+    progressBar.style.width = `${value}%`;
+  });
+}
+
+function hideProgress(){
+  progressBars.forEach(p => {
+    p.style.opacity = 0;
+    p.style.width = 0;  
+  });
+}
+
+window.addEventListener("scroll", () => {
+  const sectionPos = skillsSection.getBoundingClientRect().top;
+  const screenPos = window.innerHeight / 2;
+  
+  if(sectionPos < screenPos){
+    showProgress();
+  }else{
+    hideProgress();
+  }
+});
