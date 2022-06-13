808410fc8ca37203ce886047c83f859f8bb6c5d4
LAPTOP-HTDN1JE0\Oriiromero <oriana.nahir.r@gmai.com>
2022-06-13 11:58:43 +0200

    [css] correcting form style

--- index.js
+++ index.js
@@ -101,11 +101,3 @@ window.addEventListener("scroll", () => {
 });
 
 //CONTACT FORM//
-$('input').focus(function() {
-  $(this).parent().addClass('active');
-  $('input').focusout(function() {
-    if($(this).val() == '') { $(this).parent().removeClass('active');
-    } else { $(this).parent().addClass('active');
-    }
-  })
-});
