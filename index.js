a2b639033ac088c8ce1d09d2dd52ef5e49c304a5
LAPTOP-HTDN1JE0\Oriiromero <oriana.nahir.r@gmai.com>
2022-06-13 11:41:15 +0200

    [html] add contact section [css] start styiling contact

--- index.js
+++ index.js
@@ -101,3 +101,11 @@ window.addEventListener("scroll", () => {
 });
 
 //CONTACT FORM//
+$('input').focus(function() {
+  $(this).parent().addClass('active');
+  $('input').focusout(function() {
+    if($(this).val() == '') { $(this).parent().removeClass('active');
+    } else { $(this).parent().addClass('active');
+    }
+  })
+});
